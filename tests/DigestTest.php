30baa181dbd88d565c9fccc14816ed4836b68215
Rafael del Valle <rvalle@livelens.net>
2014-10-20 13:51:44 +0200

    first test working!

---
+++ tests/DigestTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Celpax\Dailypulse\Test;
+
+class TestDigest extends \PHPUnit_Framework_TestCase{
+    public function testDummy(){
+        $this->assertEquals(1+1,2);
+    }
+}
+
