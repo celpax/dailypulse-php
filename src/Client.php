a32e04e963222b197cd9e59fc18b8a8892ddc810
Rafael del Valle <rvalle@livelens.net>
2014-10-22 16:40:41 +0200

    updated version

--- src/Client.php
+++ src/Client.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Exception\RequestException;
 class Client {
 
     const API_ENDPOINT="https://api.celpax.com:8443";
-    const VERSION="1.0.3";
+    const VERSION="1.0.4";
     const ACCEPTS_VERSION="~1.0";
 
     const SIGNATURE_HEADER="X-Celpax-Signature";
