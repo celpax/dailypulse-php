e1490cd2b38b0e81de2d7a4e7e82c3a84e30bfcd
Sergi Espinar <sergi@celpax.com>
2016-09-29 11:52:08 +0200

    updated version number, and API endpoint

--- src/Client.php
+++ src/Client.php
@@ -7,8 +7,8 @@ use GuzzleHttp\Exception\RequestException;
 
 class Client {
 
-    const API_ENDPOINT="https://api.celpax.com:4443";
-    const VERSION="1.0.10";
+    const API_ENDPOINT="https://api.celpax.com";
+    const VERSION="1.0.11";
     const ACCEPTS_VERSION="~1.0";
 
     const SIGNATURE_HEADER="X-Celpax-Signature";
